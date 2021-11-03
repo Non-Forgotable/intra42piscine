b3e9488895de16bd7ec59736d3f52744d8ab66a0
Onur Dursun <58361733+onur55-tr@users.noreply.github.com>
2021-11-03 16:27:49 +0300

    Success: %85

---
+++ C_01/ex04/ft_is_negative.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: odursun <42istanbul.com.tr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/21 14:29:33 by odursun           #+#    #+#             */
+/*   Updated: 2021/10/23 17:26:21 by odursun          ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_is_negative(int n)
+{
+	char	pozitif;
+	char	negatif;
+
+	pozitif = 'P';
+	negatif = 'N';
+	if (n < 0)
+	{
+		write(1, &negatif, 1);
+	}
+	else
+	{
+		write(1, &pozitif, 1);
+	}
+}
